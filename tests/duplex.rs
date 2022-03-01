3065278c59233e95ad3006b7888e77c13435dd15
Griffin O'Neill <gsoneill1003@gmail.com>
2022-03-01 14:00:15 -0500

    Updated test

--- tests/duplex.rs
+++ tests/duplex.rs
@@ -14,25 +14,36 @@ fn print_line(buf_reader: &mut BufReader<Pipe>)
     stdout().flush().unwrap();
 }
 
+fn client_server1(mut pipe: Pipe) 
+{
+    writeln!(pipe, "test1").unwrap();
+    writeln!(pipe, "test2").unwrap();
+    writeln!(pipe, "test3").unwrap();
+    let mut buf_reader = BufReader::new(pipe);
+    print_line(&mut buf_reader);
+    print_line(&mut buf_reader);
+    print_line(&mut buf_reader);
+}
+
+fn client_server2(pipe: Pipe) 
+{
+    let mut buf_reader = BufReader::new(pipe);
+    print_line(&mut buf_reader);
+    print_line(&mut buf_reader);
+    print_line(&mut buf_reader);
+    let mut pipe = buf_reader.into_inner();
+    writeln!(pipe, "test4").unwrap();
+    writeln!(pipe, "test5").unwrap();
+    writeln!(pipe, "test6").unwrap();
+}
+
 #[test]
 fn duplex_test() 
 {
-    let mut pipe = Pipe::with_name("test2").unwrap();
+    let pipe = Pipe::with_name("test2").unwrap();
     let pipe_clone = pipe.clone();
-    let t1 = thread::spawn(||
-    {
-        writeln!(pipe, "test1").unwrap();
-        let mut buf_reader = BufReader::new(pipe);
-        print_line(&mut buf_reader);
-    });
-    let t2 = thread::spawn(|| 
-    {
-        let mut buf_reader = BufReader::new(pipe_clone);
-        print_line(&mut buf_reader);
-        let mut pipe_clone = buf_reader.into_inner();
-        writeln!(pipe_clone, "test2").unwrap();
-    });
+    let t1 = thread::spawn(|| client_server1(pipe));
+    let t2 = thread::spawn(|| client_server2(pipe_clone));
     t1.join().unwrap();
     t2.join().unwrap();
 }
-
